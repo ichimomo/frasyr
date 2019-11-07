32cbce38d054deaf9590b1e21fcedbcd8aef46ab
Akira Hayashi <akira.hayashi.1987@gmail.com>
2019-11-07 16:11:42 +1300

    :sparkles: Add draw_SRline_()
    To check fit of SR model to the observed SR data
    
    :bulb: Add man

--- R/sr_selection.R
+++ R/sr_selection.R
@@ -18,3 +18,25 @@ make_SR_dframe <- function(SR, method, vpares) {
     pull_df_from_list(dfname = "pred") %>%
     dplyr::mutate(name = paste0(SR, "_", method))
 }
+
+#' Draw fitted SR line over observed stock-recruitment plots
+#'
+#' @param fitted Fitted SR data created by fit.SR()
+#' @param observed Observed data in format data.frame(year, SSB, R)
+#' @param show.year If TRUE, show year label on each points
+draw_SRline_ <- function(fitted, observed, show.year) {
+  label <- ""
+  if (show.year == TRUE) label <- observed$year
+  ggplot2::ggplot(data = observed,
+                        ggplot2::aes(x = SSB,
+                                     y = R,
+                                     label = label)) +
+    ggplot2::geom_point() +
+    ggrepel::geom_text_repel() +
+    ggplot2::geom_line(data = fitted,
+                       inherit.aes = FALSE,
+                       ggplot2::aes(x = pred.SSB,
+                                    y = pred.R,
+                                    group = name,
+                                    color = name))
+}
