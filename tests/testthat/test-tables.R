368c8dd5c1ed91c9b4e0685ba29d1adb4c4a4f8a
Akira Hayashi <smxshxishxad@me.com>
2020-07-21 12:06:31 +0900

    :white_check_mark: Update test

--- tests/testthat/test-tables.R
+++ tests/testthat/test-tables.R
@@ -15,8 +15,7 @@ test_that("make_stock_table() works", {
                           result_msy        = result_msy,
                           result_future     = generate_dummy_future_new_object(),
                           yr_future_start   = 2012,
-                          yrs_preabc        = 2009:2011,
-                          yrs_biopar        = 2008:2010)
+                          faa_pre_abc       = c(0.123, 0.234, 0.345, 0.345))
 
   expect_df(tbl)
   expect_equal(colnames(tbl),
