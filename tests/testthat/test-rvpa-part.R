1dc524d2fefc2c26f71fcf66d4b460d0479d3649
Akira Hayashi <smxshxishxad@me.com>
2020-11-30 11:55:50 +0900

    :recycle: Load data at top-level

--- tests/testthat/test-rvpa-part.R
+++ tests/testthat/test-rvpa-part.R
@@ -1,11 +1,13 @@
 library(frasyr)
 
+caa   <- read.csv(system.file("extdata","caa_pma.csv",package="frasyr"),row.names=1)
+waa   <- read.csv(system.file("extdata","waa_pma.csv",package="frasyr"),row.names=1)
+maa   <- read.csv(system.file("extdata","maa_pma.csv",package="frasyr"),row.names=1)
+index <- read.csv(system.file("extdata","index_pma.csv",package="frasyr"),row.names=1)
+
 context("data input")
 
 test_that("caa, waa and maa data input error check",{
-  caa <- read.csv(system.file("extdata","caa_pma.csv",package="frasyr"),row.names=1)
-  waa <- read.csv(system.file("extdata","waa_pma.csv",package="frasyr"),row.names=1)
-  maa <- read.csv(system.file("extdata","maa_pma.csv",package="frasyr"),row.names=1)
   expect_equal(nrow(caa),nrow(waa))
   expect_equal(nrow(caa),nrow(maa))
 })
@@ -13,11 +15,6 @@ test_that("caa, waa and maa data input error check",{
 context("vpa")
 
 test_that("usage", {
-
-  caa <- read.csv(system.file("extdata","caa_pma.csv",package="frasyr"),row.names=1)
-  waa <- read.csv(system.file("extdata","waa_pma.csv",package="frasyr"),row.names=1)
-  maa <- read.csv(system.file("extdata","maa_pma.csv",package="frasyr"),row.names=1)
-  index <- read.csv(system.file("extdata","index_pma.csv",package="frasyr"),row.names=1)
   dat <- data.handler(caa = caa, waa = waa, maa = maa, M = 0.5, index = index)
   expect_error(vpa(dat, abund = "N", tune = TRUE),
                "length(abund) not equal to nrow(index)",
@@ -25,10 +22,6 @@ test_that("usage", {
 })
 
 test_that("output value check",{
-  caa <- read.csv(system.file("extdata","caa_pma.csv",package="frasyr"),row.names=1)
-  waa <- read.csv(system.file("extdata","waa_pma.csv",package="frasyr"),row.names=1)
-  maa <- read.csv(system.file("extdata","maa_pma.csv",package="frasyr"),row.names=1)
-
   dat <- data.handler(caa=caa, waa=waa, maa=maa, M=0.5)
   res_vpa_pma_check <- vpa(dat,fc.year=2009:2011,rec=585,rec.year=2011,tf.year = 2008:2010,
                        term.F="max",stat.tf="mean",Pope=TRUE,tune=FALSE,p.init=1.0)
