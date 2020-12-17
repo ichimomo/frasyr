b3fb613f77766ca42b68ebb87c1f61c875b433ec
Akira Hayashi <smxshxishxad@me.com>
2020-12-17 11:37:11 +0900

    :recycle: Replace expect_equal() with assert_that()

--- R/rvpa.r
+++ R/rvpa.r
@@ -31,15 +31,19 @@ data.handler <- function(
   if (!is.null(waa.catch)) {
     if (is.null(dim(waa.catch)) | dim(waa.catch)[2]==1) waa.catch <- as.data.frame(matrix(unlist(waa.catch), nrow=nrow(caa), ncol=ncol(caa)))
     colnames(waa.catch) <- years
-    expect_equal(rownames(caa),rownames(waa.catch))
-    expect_equal(colnames(caa),colnames(waa.catch))
+    assertthat::assert_that(
+      rownames(caa) == rownames(waa.catch),
+      colnames(caa) == colnames(waa.catch)
+    )
   }
 
   if (!is.null(maa.tune)) {
     if (is.null(dim(maa.tune)) | dim(maa.tune)[2]==1) maa.tune <- as.data.frame(matrix(unlist(maa.tune), nrow=nrow(caa), ncol=ncol(caa)))
     colnames(maa.tune) <- years
-    expect_equal(rownames(caa),rownames(maa.tune))
-    expect_equal(colnames(caa),colnames(maa.tune))
+    assertthat::assert_that(
+      rownames(caa) == rownames(maa.tune),
+      colnames(caa) == colnames(maa.tune)
+    )
   }
 
   if (!is.null(catch.prop)) colnames(catch.prop) <- years
@@ -51,13 +55,14 @@ data.handler <- function(
   colnames(M) <- years
   rownames(M) <- rownames(caa)
   
-  expect_equal(rownames(caa),rownames(maa))
-  expect_equal(rownames(caa),rownames(waa))
-  expect_equal(rownames(caa),rownames(M))
-  
-  expect_equal(colnames(caa),colnames(maa))
-  expect_equal(colnames(caa),colnames(waa))
-  expect_equal(colnames(caa),colnames(M))
+  assertthat::assert_that(
+    all(rownames(caa) == c(rownames(maa),
+                           rownames(waa),
+                           rownames(M))),
+    all(colnames(caa) == c(colnames(maa),
+                           colnames(waa),
+                           colnames(M)))
+  )
   
   res <- list(caa=caa, maa=maa, waa=waa, index=index, M=M, maa.tune=maa.tune, waa.catch=waa.catch, catch.prop=catch.prop)
 
