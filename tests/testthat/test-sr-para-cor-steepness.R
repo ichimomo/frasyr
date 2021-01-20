b38e080d499a115417a6359686517829e82648bd
Momoko Ichinokawa <ichimomo555@gmail.com>
2021-01-20 17:16:47 +0900

    共通して使うパラメータをtest関数の外に出す

--- tests/testthat/test-sr-para-cor-steepness.R
+++ tests/testthat/test-sr-para-cor-steepness.R
@@ -2,20 +2,20 @@ library(frasyr)
 
 context("check the correlation between parameters and the steepness")
 
-test_that("output value check",{
+# vpaとget.SRdataの結果を読み込み
+load(system.file("extdata","res_vpa_pma.rda",package = "frasyr"))
+load(system.file("extdata","SRdata_pma.rda",package = "frasyr"))
 
-  # vpaとget.SRdataの結果を読み込み
-  load(system.file("extdata","res_vpa_pma.rda",package = "frasyr"))
-  load(system.file("extdata","SRdata_pma.rda",package = "frasyr"))
+SRmodel.list <- expand.grid(SR.rel = c("HS","BH","RI"), AR.type = c(0, 1), out.AR=c(TRUE,FALSE), L.type = c("L1", "L2"))
 
-  SRmodel.list <- expand.grid(SR.rel = c("HS","BH","RI"), AR.type = c(0, 1), out.AR=c(TRUE,FALSE), L.type = c("L1", "L2"))
+res.SRfit <- list()
 
-  res.SRfit <- list()
-
-  # fit.SR計算
-  for (i in 1:nrow(SRmodel.list)){
+# fit.SR計算
+for (i in 1:nrow(SRmodel.list)){
     res.SRfit[[i]] <- fit.SR(SRdata = SRdata_pma,SR=SRmodel.list$SR.rel[i],method=SRmodel.list$L.type[i],AR=SRmodel.list$AR.type[i],out.AR=SRmodel.list$out.AR[i])
-    }
+}
+
+test_that("output value check",{
 
   year <- as.character(max(res_vpa_pma$input$rec.year))
 
