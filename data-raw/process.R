cb060e2bc6c7a0199a383689ff937fdf2de69b62
Momoko Ichinokaw <ichimomo555@gmail.com>
2021-12-01 12:53:45 +0900

    update process.R and related rda files (#709)

--- data-raw/process.R
+++ data-raw/process.R
@@ -7,84 +7,70 @@ maa   <- read.csv("ex2_maa.csv",  row.names=1)
 dat <- data.handler(caa=caa, waa=waa, maa=maa, M=0.5)
 
 # VPAによる資源量推定
-res_vpa <- vpa(dat,fc.year=2015:2017,tf.year = 2015:2016,
+res_vpa_example <- vpa(dat,fc.year=2015:2017,tf.year = 2015:2016,
                term.F="max",stat.tf="mean",Pope=TRUE,tune=FALSE,p.init=0.5)
-devtools::use_data(res_vpa)
+use_data(res_vpa_example, overwrite=TRUE)
 
 # SR関数のフィット
 SRdata <- get.SRdata(res_vpa)
 res_sr_HSL2 <- fit.SR(SRdata,SR="HS",method="L2",AR=0,hessian=FALSE)
-use_data(res_sr_HSL2)
+use_data(res_sr_HSL2, overwrite=TRUE)
 
 res_sr_HSL1 <- fit.SR(SRdata,SR="HS",method="L1",AR=0,hessian=FALSE)
-use_data(res_sr_HSL1)
+use_data(res_sr_HSL1, overwrite=TRUE)
 
-res_future_HSL2 <- future.vpa(res_vpa,
-                      multi=1,
-                      nyear=50, # 将来予測の年数
-                      start.year=2011, # 将来予測の開始年
-                      N=100, # 確率的計算の繰り返し回数=>実際の計算では1000~5000回くらいやってください
-                      ABC.year=2019, # ABCを計算する年
-                      waa.year=2015:2017, # 生物パラメータの参照年
-                      maa.year=2015:2017,
-                      M.year=2015:2017,
-                      is.plot=TRUE, # 結果をプロットするかどうか
-                      seed=1,
-                      silent=FALSE,
-                      recfunc=HS.recAR, # 再生産関係の関数
-                      # recfuncに対する引数
-                      rec.arg=list(a=res_sr_HSL2$pars$a,b=res_sr_HSL2$pars$b,
-                                   rho=res_sr_HSL2$pars$rho, # ここではrho=0なので指定しなくてもOK
-                                   sd=res_sr_HSL2$pars$sd,resid=res_sr_HSL2$resid))
+# normal lognormal ----
+data_future_HSL2 <- make_future_data(res_vpa_example, # VPAの結果
+                                     nsim = 100, # シミュレーション回数
+                                     nyear = 20, # 将来予測の年数
+                                     future_initial_year_name = 2017, 
+                                     start_F_year_name = 2018, 
+                                     start_biopar_year_name=2018, 
+                                     start_random_rec_year_name = 2018,
+                                     # biopar setting
+                                     waa_year=2015:2017, waa=NULL, 
+                                     waa_catch_year=2015:2017, waa_catch=NULL,
+                                     maa_year=2015:2017, maa=NULL,
+                                     M_year=2015:2017, M=NULL,
+                                     # faa setting
+                                     faa_year=2015:2017, 
+                                     currentF=NULL,futureF=NULL, 
+                                     # HCR setting (not work when using TMB)
+                                     start_ABC_year_name=2019, # HCRを適用する最初の年
+                                     HCR_beta=1, # HCRのbeta
+                                     HCR_Blimit=-1, # HCRのBlimit
+                                     HCR_Bban=-1, # HCRのBban
+                                     HCR_year_lag=0, # HCRで何年遅れにするか
+                                     HCR_function_name = "HCR_default",
+                                     # SR setting
+                                     res_SR=res_sr_HSL2, 
+                                     seed_number=1, 
+                                     resid_type="lognormal", 
+                                     resample_year_range=0, # リサンプリングの場合、残差をリサンプリングする年の範囲
+                                     bias_correction=TRUE, # バイアス補正をするかどうか
+                                     recruit_intercept=0, # 移入や放流などで一定の加入がある場合に足す加入尾数
+                                     # Other
+                                     Pope=res_vpa$input$Pope,
+                                     fix_recruit=list(year=c(2020,2021),rec=c(1000,2000)),
+                                     fix_wcatch=list(year=c(2020,2021),wcatch=c(1000,2000))
+                                     )
 
-res_MSY_HSL2 <- est.MSY(res_vpa, # VPAの計算結果
-                 res_future_HSL2$input, # 将来予測で使用した引数
-                 resid.year=0, # ARありの場合、最近何年分の残差を平均するかをここで指定する。ARありの設定を反映させたい場合必ずここを１以上とすること（とりあえず１としておいてください）。
-                 N=100, # 確率的計算の繰り返し回数=>実際の計算では1000~5000回くらいやってください
-                 calc.yieldcurve=TRUE,
-                 PGY=c(0.95,0.9,0.6,0.1), # 計算したいPGYレベル。上限と下限の両方が計算される
-                 onlylower.pgy=FALSE, # TRUEにするとPGYレベルの上限は計算しない（計算時間の節約になる）
-                 B0percent=c(0.2,0.3,0.4),
-                 Bempirical=c(round(tail(colSums(res_vpa$ssb),n=1)),
-                              round(max(colSums(res_vpa$ssb))),
-                              24000, # 現行Blimit
-                              res_sr_HSL2$pars$b) # HSの折れ点
-                 ) # 計算したいB0%レベル
+res_future_HSL2 <- future_vpa(tmb_data=data_future_HSL2$data,
+                              optim_method="none")
 
-res_future_HSL1 <- future.vpa(res_vpa,
-                      multi=1,
-                      nyear=50, # 将来予測の年数
-                      start.year=2011, # 将来予測の開始年
-                      N=100, # 確率的計算の繰り返し回数=>実際の計算では1000~5000回くらいやってください
-                      ABC.year=2019, # ABCを計算する年
-                      waa.year=2015:2017, # 生物パラメータの参照年
-                      maa.year=2015:2017,
-                      M.year=2015:2017,
-                      is.plot=TRUE, # 結果をプロットするかどうか
-                      seed=1,
-                      silent=FALSE,
-                      recfunc=HS.recAR, # 再生産関係の関数
-                      # recfuncに対する引数
-                      rec.arg=list(a=res_sr_HSL1$pars$a,b=res_sr_HSL1$pars$b,
-                                   rho=res_sr_HSL1$pars$rho, # ここではrho=0なので指定しなくてもOK
-                                   sd=res_sr_HSL1$pars$sd,resid=res_sr_HSL1$resid))
+res_MSY_HSL2 <- est_MSYRP(data_future=data_future_HSL2, candidate_PGY=c(0.1,0.6),
+                          candidate_B0=c(0.2), candidate_Babs=20000)
 
-res_MSY_HSL1 <- est.MSY(res_vpa, # VPAの計算結果
-                 res_future_HSL1$input, # 将来予測で使用した引数
-                 resid.year=0, # ARありの場合、最近何年分の残差を平均するかをここで指定する。ARありの設定を反映させたい場合必ずここを１以上とすること（とりあえず１としておいてください）。
-                 N=100, # 確率的計算の繰り返し回数=>実際の計算では1000~5000回くらいやってください
-                 calc.yieldcurve=TRUE,
-                 PGY=c(0.95,0.9,0.6,0.1), # 計算したいPGYレベル。上限と下限の両方が計算される
-                 onlylower.pgy=FALSE, # TRUEにするとPGYレベルの上限は計算しない（計算時間の節約になる）
-                 B0percent=c(0.2,0.3,0.4),
-                 Bempirical=c(round(tail(colSums(res_vpa$ssb),n=1)),
-                              round(max(colSums(res_vpa$ssb))),
-                              24000, # 現行Blimit
-                              res_sr_HSL1$pars$b) # HSの折れ点
-                 ) # 計算したいB0%レベル
+data_future_HSL1 <- redo_future(data_future_HSL2, list(res_SR=res_sr_HSL1), only_data=TRUE)
+res_MSY_HSL1 <- est_MSYRP(data_future=data_future_HSL1, candidate_PGY=c(0.1,0.6),
+                          candidate_B0=c(0.2), candidate_Babs=20000)
 
-use_data(res_MSY_HSL1)
-use_data(res_MSY_HSL2)
+use_data(res_MSY_HSL1, overwrite=TRUE)
+use_data(res_MSY_HSL2, overwrite=TRUE)
+use_data(res_future_HSL1, overwrite=TRUE)
+use_data(res_future_HSL2, overwrite=TRUE)
+use_data(data_future_HSL1, overwrite=TRUE)
+use_data(data_future_HSL2, overwrite=TRUE)
 
 # VPA dummy data
 data_base <- readr::read_csv("all_dummy_data_base.csv") 
@@ -116,5 +102,5 @@ res_vpa_estb_tune4l_b <- vpa(vpadat_estb,last.catch.zero = FALSE, min.age=c(0,0,
                              Pope = TRUE,  tune=TRUE, term.F="all", est.method="ml" ,b.est=TRUE,p.init=c(0.2,0.3,0.6,0.6),abund=c("N","N","N","N","N","N"),fc.year=1998:2000)
 
 res_vpa_estb <- res_vpa_estb_tune4l_b
-save(vpadat_estb,file="../data/vpadat_estb.rda")
-save(res_vpa_estb,file="../data/res_vpa_estb.rda")
+use_data(vpadat_estb, overwrite=TRUE)
+use_data(res_vpa_estb, overwrite=TRUE)
